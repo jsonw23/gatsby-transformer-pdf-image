adb5b1580c4970e74345c033fc2fab82b7b8e24f
Jason Williams <jason@williamstx.com>
2023-01-26 16:39:31 -0600

    Initial commit

---
+++ dist/gatsby-node.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.onPreInit = exports.onCreateNode = void 0;
+var _fs = _interopRequireDefault(require("fs"));
+var _gatsbySourceFilesystem = require("gatsby-source-filesystem");
+var _pdfImage = require("pdf-image");
+const onCreateNode = async ({
+  node,
+  actions: {
+    createNode,
+    createNodeField,
+    createParentChildLink
+  },
+  createNodeId,
+  cache
+}) => {
+  if (node.internal.mediaType === "application/pdf") {
+    let pdfNode = node;
+    let pdfImage = new _pdfImage.PDFImage(pdfNode.absolutePath, {
+      convertOptions: {
+        "-trim": ""
+      }
+    });
+    let pageImagePath = await pdfImage.convertPage(0);
+    _fs.default.readFile(pageImagePath, async (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let pageImage = await (0, _gatsbySourceFilesystem.createFileNodeFromBuffer)({
+        buffer: data,
+        createNode,
+        createNodeId,
+        cache
+      });
+      if (pageImage) {
+        createParentChildLink({
+          parent: node,
+          child: pageImage
+        });
+      }
+    });
+  }
+};
+exports.onCreateNode = onCreateNode;
+const onPreInit = async () => {
+  console.log("Loaded gatsby-transformer-pdf-image");
+};
+exports.onPreInit = onPreInit;
---
+++ src/gatsby-node.ts
@@ -0,0 +1,37 @@
+import fs from "fs"
+import { createFileNodeFromBuffer } from "gatsby-source-filesystem";
+import { PDFImage } from "pdf-image"
+
+import type { GatsbyNode } from "gatsby"
+import type { FileSystemNode } from "gatsby-source-filesystem"
+
+export const onCreateNode: GatsbyNode["onCreateNode"] = async ({ node, actions: { createNode, createNodeField, createParentChildLink }, createNodeId, cache }) => {
+    if (node.internal.mediaType === "application/pdf") {
+        let pdfNode = node as FileSystemNode
+        let pdfImage = new PDFImage(pdfNode.absolutePath, {
+            convertOptions: {
+                "-trim": ""
+            }
+        })
+        let pageImagePath = await pdfImage.convertPage(0)
+        fs.readFile(pageImagePath, async (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            let pageImage = await createFileNodeFromBuffer({
+                buffer: data,
+                createNode,
+                createNodeId,
+                cache,
+            })
+            if (pageImage) {
+                createParentChildLink({ parent: node, child: pageImage })
+            }
+        })
+    }
+}
+
+export const onPreInit: GatsbyNode["onPreInit"] = async () => {
+    console.log("Loaded gatsby-transformer-pdf-image")
+}
